e8ec89e49f3168454f857a4afffe7b7c3ec0ad35
Stejmuri <138825475+Stejmuri@users.noreply.github.com>
2025-08-06 13:44:41 -0500

    Update

--- landing_api/views.py
+++ landing_api/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
-# landing_api/views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,11 +6,25 @@ from datetime import datetime
 
 class LandingAPI(APIView):
     name = "Landing API"
-    collection_name = "landing_messages"  # nombre de la colección en Firebase
+    collection_name = "landing_messages"  # o como se llame tu colección
 
     def get(self, request):
+        producto_id = request.query_params.get('productoID', None)
         ref = db.reference(self.collection_name)
         data = ref.get()
+
+        if producto_id:
+            # Buscar coincidencias por productoID
+            resultados = {
+                key: value for key, value in data.items()
+                if value.get("productoID") == producto_id
+            }
+
+            if resultados:
+                return Response(resultados, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Producto no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
         return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request):
